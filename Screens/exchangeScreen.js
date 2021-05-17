2a7751a50158916f03c716a35eaab8a9515a1bc6
Samridh212 <70936587+Samridh212@users.noreply.github.com>
2021-05-17 15:52:13 +0530

    Add files via upload

---
+++ Screens/exchangeScreen.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert, Modal,ScrollView,KeyboardAvoidingView } from 'react-native';
+
+import db from '../config';
+import firebase from 'firebase';
+
+export default class exchangeScreen extends Component {}
---
+++ Screens/homeScreen.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert, Modal,ScrollView,KeyboardAvoidingView } from 'react-native';
+
+
+
+
+
+export default class homeScreen extends Component {}
